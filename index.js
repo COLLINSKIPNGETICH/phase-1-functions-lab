0e4b67f9e4da7bd4d3118e107d2fe1ccffa71721
+collins <+collinskipngetich60@gmail.com>
2023-12-23 15:26:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+// 
+function distanceFromHqInBlocks(pickupLocation) {
+  // Scuber's headquarters is on 42nd Street
+  const hqLocation = 42;
+  
+  // Return distance in blocks
+  const distance = Math.abs(pickupLocation - hqLocation);
+  
+  return distance;
+}
+function distanceFromHqInFeet(pickupLocation) {
+  // calculates distances below 42nd street
+  const blocks = distanceFromHqInBlocks(pickupLocation);
+  
+  // Each block is 264 feet long
+  const feet = blocks * 264;
+  
+  return feet;
+}
+function distanceTravelledInFeet(start, destination) {
+  // Calculate the distance in blocks
+  const blocks = Math.abs(destination - start);
+  
+  // Each block is 264 feet long
+  const feet = blocks * 264;
+  
+  return feet;
+}
+function calculatesFarePrice(start, destination) {
+  // Calculate the distance in feet and there charges
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  }
+  else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  }
+  else if (distance <= 2500) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far';
+  }
+}
+
